23faa880323c4c72483e174b8dae1144af852d2e
peje <pejecomics@gmail.com>
2026-09-22 14:10:42 -0500

    Polishing task class

--- task.py
+++ task.py
@@ -5,7 +5,7 @@ class task:
     def __init__(self,id,description,status):
         self.__id = id
         self.__description = description
-        self.__status = status
+        self.__status = "todo"
         self.__createdAt = datetime.datetime.now()
         self.__updatedAt = datetime.datetime.now()
     
@@ -14,3 +14,20 @@ class task:
         return self.__id
     def setID(self, id):
         self.__id = id
+    def getDesc(self):
+        return self.__description
+    #IDEA: retrieve whole description and allow user to change description partially instead of change the whole description
+    #HOW: Retrieve the saved description and allow edition to replace the description inside task object
+    def setDesc(self, description):
+        self.__description = description
+    def getStatus(self):
+        return self.__id
+    def setStatus(self, status):
+        if status == "todo":
+            print("ERROR: You can't change a task with a todo status")
+            return
+        self.__status = status
+    def getCreatedAt(self):
+        return self.__createdAt
+    def setUpdatedAt(self):
+        self.__updatedAt = datetime.datetime.now()
