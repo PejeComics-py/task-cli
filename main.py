4ce826f34af94852584e94739070487df453aa04
peje <pejecomics@gmail.com>
2026-09-24 17:23:37 -0500

    Adding file handling funcionalities.
    TODOS:
    - Make counter in Task class consider objects in .json file
    - Develop reading function to read file
    - Expand program description

--- main.py
+++ main.py
@@ -2,49 +2,82 @@ import task
 import json
 import os
 
+# Note: Finally made this work 
+def saveInFile(task):
+    json_file = "tasks.json"
+    current_path = os.path.dirname(os.path.abspath(__file__)) #Assign current path file to current_path variable
+    if os.path.isfile(os.path.join(current_path,json_file)): #Checks if the file exists, if not, jumps to createFile() and comes back to this fucntion
+        with open(json_file,"r") as stream1: 
+            file_data = json.load(stream1)
+            file_data["tasks"].append(task.__dict__)
+            with open(json_file,"w") as stream2:
+                stream2.write(json.dumps(file_data,indent=4,default=str))
+    else:
+        createFile() 
+        saveInFile(task)
+
 #Note: If .json does not exist, function creates it
 def createFile():
-    try:
-        json_file = "tasks.json"
-        fileSerialized= open(json_file,"x")
-        current_path = os.path.dirname(os.path.abspath(__file__))
-        if os.path.isfile(os.path.join(current_path,json_file)):
-            print(f"WARN: File was {json_file} correctly")
-        fileSerialized.close()
-    except FileExistsError:
+    json_file = "tasks.json"
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    count = 0
+    if not os.path.isfile(os.path.join(current_path,json_file)):
+        file = open(json_file,"w")
+        tasks = {"tasks" : []} 
+        file.write(json.dumps(tasks,indent=4,default=str)) #Default=str to cascade attributes into a str
+        print(f"WARN: File was {json_file} created correctly")
+        file.close()
+    else: 
+        count +=1 
+        if count < 1:
+            print(f"WARN: File {json_file} already exists")
         return
 
-
-#Note: Verify if .json exist and call createFile() for that, else just write objects past as *args into .json
-def saveInFile(*args):
+#INFO: read file or whatever, does not work yet 
+def readFile():
     json_file = "tasks.json"
     current_path = os.path.dirname(os.path.abspath(__file__))
     if os.path.isfile(os.path.join(current_path,json_file)):
-        fileSerialized = open(json_file,"w")
-        for arg in args: 
-            fileSerialized.write(json.dumps(arg.__dict__,indent=2,default=str))
-        fileSerialized.close()
-    else:
-        print("Creating file...")
+        extracted_objs = []
+        with open(json_file,'r') as file:
+            for line in file:
+                json_obj = json.loads(line)
+                extracted_objs.append(json_obj)
+        return extracted_objs
+    else: 
         createFile()
-        print("writing file...")
-        saveInFile(*args)
+        readFile()
 
-"""
 print("----Welcome to task-cli----")
+#Note: You may need to expand programs' description, currently it is too simple 
 print("This program looks to manage your todo in your daily life")
 print("Option list: ")
-while True:
-    choice = int(input("1)Manage your tasks\n2)Display tasks\n3)Exit\n"))
-    match choice: 
-        case 1:
-            #Mange your tasks lol
-            print 
-        case 2:
-            #Display task
-            print
-        case 3:
-            #Exit
-            print("Exiting program...")
-            break
-"""
+try:
+    while True:
+        choice = int(input("1)Add a task\n2)Delete a task\n3)Edit a task\n4)Display tasks\n5)Exit\nSelect an option: "))
+        match choice: 
+            case 1:
+                #Add a task
+                temp_desc = input("Enter description: ")
+                temp_task = task.Task(temp_desc)
+                saveInFile(temp_task)
+                print(f"Task add successfully (ID: {temp_task.getID})")
+            case 2:
+                #Delete a task
+                print(readFile())
+
+                continue
+            case 3:
+                #Edit a task
+                continue
+            case 4:
+                #Display tasks
+                continue
+            case _: 
+                #Exit program
+                print("Exiting program...")
+                break
+except KeyboardInterrupt:
+    print("\nERROR: keyboard interrupt detected, exiting program...")
+
+
--- task.py
+++ task.py
@@ -1,38 +1,43 @@
 import datetime
 
-class task:
+class Task:
     # Apparently, self is calling the object in python
-    def __init__(self,id,description):
-        self.__id = id
+    #Note: Need to asociate counter with the instances created with the tasks.json, once you close the program counter starts again, not really useful
+    counter = 1
+    def __init__(self,description):
+        self.__id = Task.counter
         self.__description = description
         self.__status = "todo"
         self.__createdAt = datetime.datetime.now()
         self.__updatedAt = datetime.datetime.now()
-    
-
+        Task.counter +=1
+    @property
     def getID(self): 
         return self.__id
-    def setID(self, id):
-        self.__id = id
+    @property
     def getDesc(self):
         return self.__description
     #IDEA: retrieve whole description and allow user to change description partially instead of change the whole description
     #HOW: Retrieve the saved description and allow edition to replace the description inside task object
+    @property
     def setDesc(self, description):
         self.__description = description
+    @property
     def getStatus(self):
-        return self.__id
+        return self.__status
+    @property
     def setStatus(self, status):
         if status == "todo":
             print("ERROR: You can't change a task with a todo status")
             return
         self.__status = status
+    @property
     def getCreatedAt(self):
         return self.__createdAt
+    @property
     def setUpdatedAt(self):
         self.__updatedAt = datetime.datetime.now()
 
-    #Like the toString() method in java, changes the representation of an object
-
+    #INFO: Like the toString() method in java, changes the representation of an object
     def __repr__(self):
         return '{\n\tid: %d,\n\tdescription: %s,\n\tstatus: %s,\n\tcreated at: %s,\n\tupdated at: %s\n}'%(self.__id,self.__description,self.__status,self.__createdAt,self.__updatedAt)
